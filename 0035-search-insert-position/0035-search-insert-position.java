718ab5768b78bb39b8032ff8b272698120025957
Mamilla Druva Chandra <mamilladruva0007@gmail.com>
2025-12-28 20:15:25 +0530

    Time: 1 ms (2.31%), Space: 44.8 MB (21.88%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int s = 0;
-        int end = nums.length-1;
+        for(int i=1;i<=nums.length;i++){
+            if(nums[i-1]==target){
+                return i-1;
+            }
+            else if(nums[i-1]<target){
+                if(i<nums.length && nums[i]>target){
+                     return i;
 
-        while (s <= end) {
-            int mid = s + (end-s)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else s = mid+1;
+                }
+                
+            }
         }
-
-        return s;
-    }
+        if(target<=nums[0]){
+            return 0;
+        }
+        else{
+            return nums.length;
+        }
+}
 }

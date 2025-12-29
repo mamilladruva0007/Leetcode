fa40165938352050d10f3907c069bdc79e9faac0
Mamilla Druva Chandra <mamilladruva0007@gmail.com>
2025-12-29 11:00:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.56%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int a = 1; 
+        int b = 1; 
+        for (int i = 2; i <= n; i++) {
+            int t = a + b; 
+            a = b;               
+            b = t;             
+        }
+        return b;
+    }
+}

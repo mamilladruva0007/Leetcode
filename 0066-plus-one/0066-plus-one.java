0976ba70faafb5b7620cb29b2d62a46ccd3e3dc0
Mamilla Druva Chandra <mamilladruva0007@gmail.com>
2026-02-06 20:31:17 +0530

    Time: 0 ms (100%), Space: 43.4 MB (55.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for (int i = n-1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
+        if (digits[n - 1] < 9) {
+            digits[n - 1] += 1;
+            return digits;
+        } else {
+            for (int i = n - 1; i >= 0; i--) {
+                if (digits[i] == 9) {
+                    digits[i] = 0;  
+                } else {
+                    digits[i] += 1; 
+                    return digits;
+                }
             }
-            digits[i] = 0;
         }
         int[] result = new int[n + 1];
         result[0] = 1;
